fe560a227ff3e526d177290622d09d8ed0e1bdb1
yochen20 <d87487575107@gmail.com>
2019-04-16 18:28:43 +0800

    C#

---
+++ WarnsDorff/WarnsDorff/Step.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarnsDorff
+{
+    public class Step
+    {
+        public Step(int ctr, int i)
+        {
+            this.ctr = ctr;
+            this.i = i;
+        }
+
+        public int ctr { get; set; }
+        public int i { get; set; }
+    }
+}
